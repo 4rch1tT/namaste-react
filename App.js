8feb7a115124b8a865f61a1e272034cd8ec4a008
Archit Anirudhan <architanirudhan123@gmail.com>
2025-12-17 15:44:55 +0530

    fourth commit

--- App.js
+++ App.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 
 // Jsx syntax -> not html but React.createElement which is converted by the transpiler(babel) in the bundler to React element(object) which is then converted to html by .render method
 
-// React Element
-const heading = <h1 id="heading">Namste React 🦥</h1>;
+/* React Element
+ * const heading = <h1 id="heading">Namste React 🦥</h1>;
+ */
+
+const Title = () => <h1>Namaste React 🍺</h1>;
 
 // React Functional Component -> a function that return React elements(JSX)
+// Component Composition -> composing 2 components together
 const HeadingComponent = () => {
-  return <h1>Namaste From Functional Component</h1>;
+  return (
+    <div>
+      <Title />
+      <h2>Namaste From Functional Component</h2>
+    </div>
+  );
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
