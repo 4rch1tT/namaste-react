30ac3257c0bd8da4ea465d4bd80aa45df18ca8e4
Archit Anirudhan <architanirudhan123@gmail.com>
2026-01-23 20:25:16 +0530

    37th commit

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,11 +1,26 @@
+// unit testing
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component", () => {
-  render(<Contact />);
+// describe -> used for grouping
+describe("Contact Us page test case", () => {
+  test("Should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
+    // This is known as assertion ⇩⇩⇩
+    expect(heading).toBeInTheDocument();
+  });
 
-  expect(heading).toBeInTheDocument();
+  // instead of test we can use "it" as well(it is common practice)
+  it("Should load 2 input boxes on the Contact component", () => {
+    render(<Contact />);
+
+    // when we try to add more than one html element it acts as an array and they are react elements(objects).That's why we are using .length
+
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    expect(inputBoxes.length).not.toBe(3);
+  });
 });
